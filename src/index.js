1fe4e382de70eb9a9a1baf0a209a3d22099b4605
Mina Chuong <mina.chuong@gmail.com>
2017-09-07 16:24:14 -0700

    feat: Use low-level Redux methods to change state in store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import calendar from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  calendar,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <App store={ store }/>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
